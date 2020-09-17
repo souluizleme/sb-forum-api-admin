5b30fdaebe1021826c72609c15d897707e52258c
Luiz Leme <luiz.lheme@gmail.com>
2020-09-17 20:59:12 -0300

    Carga inicial com o projeto de Spring Boot Admin para monitoramento

---
+++ spring-boot-forum-admin/src/main/java/br/com/lheme/springbootforumadmin/SpringBootForumAdminApplication.java
@@ -0,0 +1,18 @@
+package br.com.lheme.springbootforumadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class SpringBootForumAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootForumAdminApplication.class, args);
+	}
+
+}
---
+++ spring-boot-forum-admin/src/test/java/br/com/lheme/springbootforumadmin/SpringBootForumAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lheme.springbootforumadmin;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootForumAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
